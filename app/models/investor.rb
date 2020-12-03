0993fe21464c66ceb66ac0041032a4af093cde0f
k-will22 <thestreetkw@hotmail.com>
2020-12-03 18:16:52 -0500

    uniqueness of username

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -2,5 +2,7 @@ class Investor < ApplicationRecord
     has_many :portfolios
     has_many :stocks, through: :portfolios
 
+    validates_uniqueness_of :username, message: "is already taken"
+
     has_secure_password
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,5 @@
 class Stock < ApplicationRecord
     has_many :portfolios
     has_many :investors, through: :portfolios
+
 end
